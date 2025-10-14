45b4bb7a2f4798fe000fba76931aa827f3e24732
rishank14 <rishankkalra@gmail.com>
2025-10-14 22:46:22 +0530

    .

--- src/components/ui/password-input.tsx
+++ src/components/ui/password-input.tsx
@@ -5,37 +5,35 @@ import { Input } from "./input";
 import { Eye, EyeOff } from "lucide-react";
 import { cn } from "@/lib/utils";
 
-interface PasswordInputProps
-   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type"> {}
+const PasswordInput = forwardRef<
+   HTMLInputElement,
+   Omit<React.InputHTMLAttributes<HTMLInputElement>, "type">
+>(({ className, ...props }, ref) => {
+   const [show, setShow] = useState(false);
 
-const PasswordInput = forwardRef<HTMLInputElement, PasswordInputProps>(
-   ({ className, ...props }, ref) => {
-      const [show, setShow] = useState(false);
-
-      return (
-         <div className="relative">
-            <Input
-               {...props}
-               ref={ref}
-               type={show ? "text" : "password"}
-               className={cn("pr-10", className)}
-            />
-            <button
-               type="button"
-               onClick={() => setShow((prev) => !prev)}
-               className="absolute right-2 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-foreground"
-               tabIndex={-1}
-            >
-               {show ? (
-                  <EyeOff className="w-4 h-4 transition duration-200" />
-               ) : (
-                  <Eye className="w-4 h-4 transition duration-200" />
-               )}
-            </button>
-         </div>
-      );
-   }
-);
+   return (
+      <div className="relative">
+         <Input
+            {...props}
+            ref={ref}
+            type={show ? "text" : "password"}
+            className={cn("pr-10", className)}
+         />
+         <button
+            type="button"
+            onClick={() => setShow((prev) => !prev)}
+            className="absolute right-2 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-foreground"
+            tabIndex={-1}
+         >
+            {show ? (
+               <EyeOff className="w-4 h-4 transition duration-200" />
+            ) : (
+               <Eye className="w-4 h-4 transition duration-200" />
+            )}
+         </button>
+      </div>
+   );
+});
 
 PasswordInput.displayName = "PasswordInput";
 
